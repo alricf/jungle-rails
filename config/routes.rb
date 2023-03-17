b387449c60feb68cfc2bfa32ac37066c64b93c8b
ACF <alricfernandes11@gmail.com>
2023-03-17 13:01:28 -0400

    Adding routes for user signup along with appropriate database update

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,11 @@ Rails.application.routes.draw do
     resources :categories, except: [:edit, :update, :show, :destroy]
   end
 
+  # Route to get form to register user
+  get '/signup' => 'users#new'
+  # Route to post data from form given by user
+  post '/users' => 'users#create'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
