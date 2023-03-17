5050a583c080320c1ed1e40b9389dcbeb2779957
ACF <alricfernandes11@gmail.com>
2023-03-17 15:13:22 -0400

    Updating create and destroy methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,21 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by_email(params[:email])
+    # If user exists AND password entered is correct.
+    if user && user.authenticate(params[:password])
+      # Save user id inside the browser cookie.
+      session[:user_id] = user.id
+      redirect_to '/'
+    else
+    # If user login doesn't work then go back to login form.
+      redirect_to '/login'
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to '/login'
   end
   
 end
