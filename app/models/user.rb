d7844771f71ee2cc49f81117a79091fa4de349d6
ACF <alricfernandes11@gmail.com>
2023-03-29 15:48:50 -0400

    Updating model for edge cases

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,12 @@ class User < ActiveRecord::Base
   has_secure_password
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :email, presence: true
   validates :password, presence: true, length: { minimum: 3 }
   validates :password_confirmation, presence: true
-  validates_uniqueness_of :email, case_sensitive: false
+  validates :email, presence: true, :uniqueness => {:case_sensitive => false}
 
   def self.authenticate_with_credentials(email, password)
-    user = User.find_by_email(email)
+    user = User.find_by_email(email.strip.downcase)
     # If user exists AND password entered is correct.
     if user && user.authenticate(password)
       return user
