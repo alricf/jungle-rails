2e146be9575a93da623283344db50ce1b7941856
ACF <alricfernandes11@gmail.com>
2023-03-31 12:15:42 -0400

    Adding tests for product details page

---
+++ cypress/integration/product_details.spec.js
@@ -0,0 +1,14 @@
+describe("Product Details", () => {
+  it("Visit the home page", () => {
+    cy.visit('/');
+  });
+
+  it("Clicks the scented blade product on the home page", () => {
+    cy.get('img[alt="Scented Blade"]').click();
+  });
+
+  it("The redirected page contains decription for Scented Blade product", () => {
+    cy.contains('The Scented Blade is an extremely rare, tall plant and can be found mostly in savannas. It blooms once a year, for 2 weeks.');
+  });
+
+});
